e52864bb991fa6f3e672d8cfc80519cfd581e2b0
null <null@users.noreply.github.com>
2022-11-04 11:15:07 +0100

    update to reflect changes in  moodle-theme_contentmodifier

--- config.php
+++ config.php
@@ -36,7 +36,7 @@ $THEME->yuicssmodules = [];
 $THEME->enable_dock = false;
 $THEME->editor_sheets = [];
 
-$THEME->rendererfactory = 'theme_overridden_renderer_factory';
+$THEME->rendererfactory = 'theme_extended_overridden_renderer_factory';
 $THEME->requiredblocks = ''; #wichtig, sonst geht h5p nicht
 $THEME->usefallback = true;
 $THEME->haseditswitch = true;
